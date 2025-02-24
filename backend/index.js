347127c976a0f90a8fe881d635979ca923d91656
Prince Singh <Princesinghps1619@gmail.com>
2025-02-24 15:54:58 +0530

    updated live links

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
-const express = require('express')
 const cors = require('cors')
+const express = require('express')
 const cookieParser = require("cookie-parser")
 require('dotenv').config();
 const connectDB = require('./config/db')
@@ -8,11 +8,11 @@ const { default: mongoose } = require('mongoose');
 
 const app = express();
 app.use(cors(
-    // {
-    //     origin: process.env.FRONTEND_URL,
-    //     method: ["POST", "GET", "PUT", "DELETE"],
-    //     credentials: true
-    // },
+    {
+        origin: process.env.FRONTEND_URL,
+        method: ["POST", "GET", "PUT", "DELETE"],
+        credentials: true
+    },
 ))
 app.use(express.json({ limit: '10mb' })) // photo should be under limit mb
 mongoose.connect(process.env.MONGODB_URI)
